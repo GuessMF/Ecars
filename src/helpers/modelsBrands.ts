5264c23062011944217ad8af3bbde5bdf84f34c1
Sergey <Segas95@yandex.ru>
2023-11-02 19:49:27 +0300

    33

--- src/helpers/modelsBrands.ts
+++ src/helpers/modelsBrands.ts
@@ -15,21 +15,155 @@ const carData: CarData = {
   brands: [
     {
       name: "Acura",
-      models: [{name: "1"}, {name: "2"}, {name: "3"}],
+      models: [{name: "MDX"}, {name: "RDX"}, {name: "RSX"}],
+    },
+    {
+      name: "Alfa Romeo",
+      models: [
+        {name: "Giulietta"},
+        {name: "Giullia"},
+        {name: "RS 7"},
+        {name: "RS Q8"},
+      ],
     },
     {
       name: "Audi",
       models: [{name: "A8"}, {name: "RS 6"}, {name: "RS 7"}, {name: "RS Q8"}],
     },
     {
-      name: "Alfa Romeo",
-      models: [{name: "Giulietta"}, {name: "Giullia"}],
+      name: "Aston Martin",
+      models: [{name: "DBX"}, {name: "V8"}, {name: "DB 9"}],
     },
     {
-      name: "Aston Martin",
-      models: [{name: "DBX"}, {name: "V8"}, {name: "Escape"}],
+      name: "Bentley",
+      models: [
+        {name: "Continental GT"},
+        {name: "Bentayga"},
+        {name: "Flying Spur"},
+        {name: "Mulsanne"},
+      ],
     },
-    // Добавьте другие марки и их модели по аналогии
+    {
+      name: "BMW",
+      models: [
+        {name: "3"},
+        {name: "5"},
+        {name: "6"},
+        {name: "7"},
+        {name: "M3"},
+        {name: "M5"},
+        {name: "X3"},
+        {name: "X5"},
+        {name: "X7"},
+      ],
+    },
+    {
+      name: "Ferrari",
+      models: [
+        {name: "California"},
+        {name: "Roma"},
+        {name: "F8"},
+        {name: "Portofino"},
+      ],
+    },
+    {
+      name: "Ford",
+      models: [
+        {name: "Mustang"},
+        {name: "F-150 Raptor"},
+        {name: "Bronco"},
+        {name: "Shelby"},
+      ],
+    },
+    {
+      name: "Infiniti",
+      models: [{name: "QX80"}, {name: "QX60"}],
+    },
+    {
+      name: "Jaguar",
+      models: [
+        {name: "E-Type"},
+        {name: "F-Type"},
+        {name: "F-Pace"},
+        {name: "E-Pace"},
+      ],
+    },
+    {
+      name: "Land Rover",
+      models: [
+        {name: "Range Rover"},
+        {name: "Defender"},
+        {name: "Range Rover Velar"},
+      ],
+    },
+    {
+      name: "Lexus",
+      models: [{name: "LX600"}, {name: "LX570"}, {name: "LX500"}],
+    },
+    {
+      name: "Maserati",
+      models: [
+        {name: "Quattroporte"},
+        {name: "Grecale"},
+        {name: "GranTurismo"},
+      ],
+    },
+    {
+      name: "Maybach",
+      models: [{name: "62"}, {name: "57"}],
+    },
+    {
+      name: "Mercedes Benz",
+      models: [
+        {name: "G-Klasse"},
+        {name: "S-Klasse"},
+        {name: "Maybach S-Klasse"},
+        {name: "GLS"},
+      ],
+    },
+    {
+      name: "Nissan",
+      models: [{name: "GT-R"}, {name: "Skyline GT-R"}, {name: "Patrol"}],
+    },
+    {
+      name: "Porsche",
+      models: [
+        {name: "911"},
+        {name: "Cayenne"},
+        {name: "Macan"},
+        {name: "Panamera"},
+      ],
+    },
+    {
+      name: "Rolls Royce",
+      models: [
+        {name: "Cullinan"},
+        {name: "Ghost"},
+        {name: "Phantom"},
+        {name: "Wraith"},
+      ],
+    },
+    {
+      name: "Tesla",
+      models: [
+        {name: "Model 3"},
+        {name: "Model S"},
+        {name: "Model Y"},
+        {name: "Model X"},
+      ],
+    },
+    {
+      name: "Toyota",
+      models: [{name: "Land Cruiser 300"}, {name: "Land Cruiser 200"}],
+    },
+    {
+      name: "Volkswagen",
+      models: [{name: "Multivan"}, {name: "Touareg"}],
+    },
+    {
+      name: "Volvo",
+      models: [{name: "XC90"}, {name: "S90"}, {name: "XC70"}, {name: "S80"}],
+    },
   ],
 };
 
