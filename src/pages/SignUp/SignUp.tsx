8d7b9c4e0053f0e4ddfe70f4b422306077c2814e
Sergey <Segas95@yandex.ru>
2023-11-13 18:44:45 +0300

    wwwww

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -2,6 +2,14 @@ import React, {useRef, useEffect, useState} from "react";
 import style from "./__signUp.module.scss";
 import {ReactComponent as GoogleIcon} from "../../assets/icons/google_icon.svg";
 import {NavLink} from "react-router-dom";
+import {v4 as uuidv4} from "uuid";
+interface Errors {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  checkBox?: string;
+}
 
 export default function SignUp() {
   const [name, setName] = useState<string>("");
@@ -9,6 +17,10 @@ export default function SignUp() {
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [checkBox, setCheckBox] = useState<boolean>(false);
+  const [formErrors, setFormErrors] = useState<Errors>({});
+
+  const errors: Errors = {};
+
   useEffect(() => {
     console.log("Name: " + name);
     console.log("Email: " + email);
@@ -17,18 +29,95 @@ export default function SignUp() {
     console.log("Checkbox: " + checkBox);
   });
 
+  const generateNewId = (): string => {
+    return uuidv4();
+  };
+  const submit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name) {
+      errors.name = "Поле Имя обязательно для заполнения";
+    }
+
+    if (!email) {
+      errors.email = "Поле Емэйл обязательно для заполнения";
+    }
+    if (!password) {
+      errors.password = "Поле Пароль обязательно для заполнения";
+    }
+    if (password !== confirmPassword) {
+      errors.confirmPassword = "Пароли не совпадают";
+    }
+    if (!checkBox) {
+      errors.checkBox = "Нет чекбокса";
+    }
+    console.log(errors);
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      alert("Заполните поля");
+    } else {
+      const newId = generateNewId();
+      const currentDate: Date = new Date();
+
+      const user = {
+        id: newId,
+        dateAdded: currentDate,
+        name: name,
+        email: email,
+        password: password,
+      };
+
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      };
+
+      fetch("https://65378b85bb226bb85dd365a6.mockapi.io/users", requestOptions)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .then((json) => {
+          console.log("Объект успешно отправлен на сервер:", json);
+          // setSent(true);
+          // setLoading(false);
+          // setTimeout(() => {
+          //   setSent(false);
+          // }, 2000);
+        })
+        .catch((error) => {
+          console.error("Ошибка при отправке объекта на сервер:", error);
+        });
+
+      setName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setCheckBox(false);
+
+      console.log("SUBMITTED");
+    }
+  };
+  //<div className={formErrors.name ? `${style.name} error` : style.name}>
   return (
     <div className={style.signUp}>
       <div className={style.wrapper}>
         <h1>Sing Up</h1>
 
-        <form className={style.form}>
+        <form className={style.form} onSubmit={submit}>
           <div className={style.name}>
             <span>Full name</span>
             <input
               type="name"
               placeholder="John Doe"
+              value={name}
               onChange={(e) => setName(e.target.value)}
+              className={`${formErrors.name ? style.error : ""}`}
             />
           </div>
 
@@ -36,8 +125,10 @@ export default function SignUp() {
             <span>Email address</span>
             <input
               type="email"
+              value={email}
               placeholder="example@mail.com"
               onChange={(e) => setEmail(e.target.value)}
+              className={`${formErrors.email ? style.error : ""}`}
             />
           </div>
 
@@ -47,8 +138,10 @@ export default function SignUp() {
             </div>
             <input
               type="password"
+              value={password}
               placeholder="Your password"
               onChange={(e) => setPassword(e.target.value)}
+              className={`${formErrors.password ? style.error : ""}`}
             />
             <span className={style.limit}>At least 8 characters</span>
           </div>
@@ -59,20 +152,32 @@ export default function SignUp() {
             </div>
             <input
               type="password"
+              value={confirmPassword}
               placeholder="Re-type password"
               onChange={(e) => setConfirmPassword(e.target.value)}
+              className={`${formErrors.confirmPassword ? style.error : ""}`}
             />
           </div>
 
-          <div className={style.termsPolicy}>
-            <input type="checkbox" name="" id="" />
+          <div
+            className={formErrors.checkBox ? style.error : style.termsPolicy}
+          >
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              checked={checkBox}
+              onChange={() => setCheckBox(!checkBox)}
+            />
             <div>
               I agree to the <span>Terms of Service</span> and{" "}
               <span>Privacy Policy</span>
             </div>
           </div>
 
-          <button className={style.signUpBtn}>Sign Up</button>
+          <button type="submit" className={style.signUpBtn}>
+            Sign Up
+          </button>
 
           <div className={style.login}>
             Already have an account?
